a567515cbfd4ff60a6db6497db7d25979238d872
ManpreetKaur-321 <manpreetkaur.gty@gmail.com>
2025-07-18 22:16:16 +0500

    starting of the project

--- together_culture2/together_culture2/settings.py
+++ together_culture2/together_culture2/settings.py
@@ -75,10 +75,29 @@ WSGI_APPLICATION = 'together_culture2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         password:
+#         host:
+#         username:
+#         port:
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'together_culture2',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',      # or your DB server IP
+        'PORT': '3306',           # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
